72957d8d966181c22bea60976dc51ec40310703b
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-05 19:54:26 +0600

    #12: Highly divisible triangular number

--- Project Euler #12: Highly divisible triangular number.py
+++ Project Euler #12: Highly divisible triangular number.py
@@ -1,1 +1,11 @@
 // #12: Highly divisible triangular number
+
+def f(L, nMax=45000):
+    d = [0]*nMax
+    for n in range(1, nMax):
+        for j in range(n, nMax, n):
+            d[j]+= 1
+        nDiv = d[n-1]*d[n//2] if n%2==0 else d[(n-1)//2]*d[n]
+        if nDiv > L: return (n-1)*n//2
+for _ in range(int(input())):
+    print (f(int(input())))
