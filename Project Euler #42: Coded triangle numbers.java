568269cd7db794d126990dbfe431eaa40c8d3e8b
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-06 23:54:19 +0600

    Update Project Euler #42: Coded triangle numbers.java

--- Project Euler #42: Coded triangle numbers.java
+++ Project Euler #42: Coded triangle numbers.java
@@ -1,1 +1,30 @@
 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner in = new Scanner(System.in);
+        int numberOfTestCases = in.nextInt();
+        for(int i=0;i<numberOfTestCases;i++)
+        {
+            long n = in.nextLong();
+            long perfectSquareNum = 8*n+1;
+            if(isPerfectSquare(perfectSquareNum))
+            {
+                double term = 0.5*(Math.sqrt(perfectSquareNum))-0.5;
+                System.out.println((long)term);
+            }
+            else System.out.println("-1");
+        }
+    }
+    
+    private static boolean isPerfectSquare(long n)
+    {
+        long root = (long)Math.sqrt(n);
+        return n == root*root;
+    }
+}
