f856f85c15b86b6725d78b6b9ae701b529bce6d0
Count Dracul Yeamin the Scorpio <34183654+mushfiqur47@users.noreply.github.com>
2024-01-27 12:03:08 +0600

    Update Project Euler #250: 250250.py

--- Project Euler #250: 250250.py
+++ Project Euler #250: 250250.py
@@ -1,1 +1,21 @@
+#Slow and cant pass all the test case but worked.
 
+
+def compute(n, k):
+
+    subsets, offsets = [0]*k, [0]*k
+
+    for i in range(1,n+1):
+        subsets[pow(i, i, k)] += 1
+
+    offsets[0] = 1
+    for i in range(k):
+        for j in range(subsets[i]):
+            offsets = [(offsets[j] + offsets[(j-i)]) % 10**9 for j in range(k)]
+
+    return str(offsets[0]-1)
+
+if __name__ == "__main__":
+    
+    n, k = map(int,input().split(" "))
+    print(compute(n, k))
